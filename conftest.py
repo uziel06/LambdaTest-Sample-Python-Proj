aae396840f6129cca6cc335b5f8cc53754b6d559
uziel06 <57972277+uziel06@users.noreply.github.com>
2023-01-16 21:25:29 +0400

    Initial Commit

---
+++ conftest.py
@@ -0,0 +1,95 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains as AC
+
+#Setup to execute on LambdaTest Platform and for Cross-browser testing
+username = "" # Replace the username with your lambdatest username
+access_key = "" # Replace the access key with your lambdatest access key that can be found after registering to their platform
+
+#Setup to run in Chrome and Windows 10 OS
+@pytest.fixture(scope="class")
+def setup_chrome(request):
+    chromeOptions = webdriver.ChromeOptions()
+    chrome_options = {
+        "username": username,
+        "accessKey": access_key,
+        "platform": "Windows 10",
+        "browserName": "Chrome",
+        "version": "86.0",
+        "visual": True,
+        "video": True,
+        "resolution": "1280x1024",
+        "network": True,
+        "build": "AssignmentTask",
+        "name": "ChromeWindowsExecution",
+        "project": "MySeleniumCertification",
+        "console": "error",
+        "w3c": True,
+        "plugin": "python-python",
+        "selenium_version": "4.0.0",
+    }
+    chromeOptions.set_capability('LT:Options', chrome_options)
+    driver = webdriver.Remote(
+        command_executor="http://{}:{}@hub.lambdatest.com/wd/hub".format(
+            username, access_key), options = chromeOptions)
+
+    driver.set_page_load_timeout(20)
+    driver.maximize_window()
+    driver.get("https://www.lambdatest.com/")
+    scrolltoAllIntegration = driver.find_element(By.CSS_SELECTOR, "div[class*='clearfix'] a[href*='https://www.lambdatest.com/integrations']")
+    wait = WebDriverWait(driver, 5)
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[class*='clearfix'] a[href*='https://www.lambdatest.com/integrations']")))
+    action = AC(driver)
+    action.move_to_element(scrolltoAllIntegration)
+    action.perform()
+
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+#Setup to run in Microsoft Edge and macOS
+@pytest.fixture(scope="class")
+def setup_edge(request):
+    edgeOptions = webdriver.EdgeOptions()
+    edge_options = {
+        "username": username,
+        "accessKey": access_key,
+        "platform": "macOS Sierra",
+        "browserName": "Edge",
+        "version": "87.0",
+        "visual": True,
+        "video": True,
+        "resolution": "1280x1024",
+        "network": True,
+        "build": "AssignmentTask",
+        "name": "EdgeMacOSExecution",
+        "project": "MySeleniumCertification",
+        "console": "error",
+        "w3c": True,
+        "plugin": "python-python",
+        "selenium_version": "4.0.0",
+    }
+    edgeOptions.set_capability('LT:Options', edge_options)
+    driver = webdriver.Remote(
+        command_executor="http://{}:{}@hub.lambdatest.com/wd/hub".format(
+            username, access_key), options = edgeOptions)
+
+    driver.set_page_load_timeout(20)
+    driver.maximize_window()
+    driver.get("https://www.lambdatest.com/")
+    scrolltoAllIntegration = driver.find_element(By.CSS_SELECTOR, "div[class*='clearfix'] a[href*='https://www.lambdatest.com/integrations']")
+    wait = WebDriverWait(driver, 5)
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[class*='clearfix'] a[href*='https://www.lambdatest.com/integrations']")))
+    action = AC(driver)
+    action.move_to_element(scrolltoAllIntegration)
+    action.perform()
+
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+
+
